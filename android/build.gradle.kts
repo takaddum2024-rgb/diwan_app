fe232724283f15e83f3732638a6d9f1c42e195aa
takaddum2024-rgb <takaddum.2024@gmail.com>
2026-05-02 13:56:19 +0300

    Update build.gradle.kts

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,3 +1,16 @@
+buildscript {
+    val kotlin_version by extra("1.9.0")
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        // تحديث أداة بناء الأندرويد لنسخة متوافقة
+        classpath("com.android.tools.build:gradle:8.1.0")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version")
+    }
+}
+
 allprojects {
     repositories {
         google()
